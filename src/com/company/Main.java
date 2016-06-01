9dd1a91a461d6fbce8015a36d4177206ecfeac2d
Gleb Maltsev <Gleb.maltsev@effective-soft.com>
2016-06-01 14:54:09 +0300

    Add Russian comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,4 @@ public class Main {
     public static void main(String[] args) {
         TestProvider.verifyPentaDiagonalSolver();
     }
-
-    private static double[][] initMatrix(int N) {
-        return new double[N + 1][N + 1];
-    }
-
-
-    private static double[] initF(int N) {
-        return new double[N + 1];
-    }
 }
